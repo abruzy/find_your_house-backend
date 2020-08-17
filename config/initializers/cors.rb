05723309ad603e56cab19eaa9fe753a892debfaf
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-08-17 17:32:03 +0100

    .

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'http://localhost:3000'
-  #   resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
-  #   credentials: true
-  # end
+  allow do
+    origins 'http://localhost:3000'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
+    credentials: true
+  end
 
   allow do
-    origins '*'
+    origins 'https://findhouse-app-api.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
     credentials: true
   end
