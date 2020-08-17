c9e733e39aef9bd2b72859349ecafd690ffcf903
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-08-17 22:06:35 +0100

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :listings, only: %i[index show]
+  resources :listings, only: %i[index show create]
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
